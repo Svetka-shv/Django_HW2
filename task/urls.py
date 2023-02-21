2cd1cc48100a1af63b644bf707dd2a957421d1b0
Svetka-shv <75161365+Svetka-shv@users.noreply.github.com>
2023-02-21 11:01:00 +0200

    add hw30-31

--- task/urls.py
+++ task/urls.py
@@ -3,9 +3,10 @@ Task application URL Configuration
 """
 
 from django.urls import path
-from task.views import task_list_view, task_detail_view
+from task.views import task_list_view, task_detail_view, category_view
 
 urlpatterns = [
     path("", task_list_view, name="task-list"),
-    path("<int:id>/", task_detail_view, name="task-detail")
+    path("<int:pk>/", task_detail_view, name="task-detail"),
+    path("<str:category>/", category_view, name="category-view")
 ]
--- task/views.py
+++ task/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse, Http404
-
-# Create your views here.
-
-tasks = [{"id": 1, "title": "Task #1", "description": "Some long task description", "created": "2022-01-24 12:00", "updated": "2022-01-25 12:00", "completed": False},
-    {"id": 2, "title": "Task #2", "description": "Some long task description", "created": "2022-01-24 12:00", "updated": "2022-01-25 12:00", "completed": True},
-    {"id": 3, "title": "Task #3", "description": "Some long task description", "created": "2022-01-24 12:00", "updated": "2022-01-25 12:00", "completed": False},
-    {"id": 4, "title": "Task #4", "description": "Some long task description", "created": "2022-01-24 12:00", "updated": "2022-01-25 12:00", "completed": True},
-    {"id": 5, "title": "Task #5", "description": "Some long task description", "created": "2022-01-24 12:00", "updated": "2022-01-25 12:00", "completed": False}
-]
+from django.http import HttpRequest, HttpResponse
+from task.models import Task
+from django.http import Http404
 
 
 def task_list_view(request: HttpRequest):
-    return render(request, 'task_list.html', {"tasks": tasks})
+    ctx ={
+        "object_list": Task.objects.order_by("category"),
+    }
+    return render(request, 'task_list.html', ctx)
 
-def task_detail_view(request: HttpRequest, id: int) -> HttpResponse:
-    for task in tasks:
-        if task["id"] == id:
-            return render(request, 'task_detail.html', {"task": task})
-    raise Http404
+def task_detail_view(request: HttpRequest, pk: int) -> HttpResponse:
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        raise Http404
+    ctx = {
+        "object": task,
+    }
+    return render(request, 'task_detail.html', ctx)
+
+def category_view(request: HttpRequest, category) -> HttpResponse:
+    ctx = {
+        "object_category": Task.objects.filter(category=category)
+    }
+    return render(request, 'category.html', ctx)
